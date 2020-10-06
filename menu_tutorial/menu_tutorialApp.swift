0c877035a2d2d5af0fba3415b3b2193ac1e0c155
Jeff, Jeong  정 의정 <35421421+TuenTuenna@users.noreply.github.com>
2020-10-07 01:23:23 +0900

    menu

---
+++ menu_tutorial/menu_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  menu_tutorialApp.swift
+//  menu_tutorial
+//
+//  Created by Jeff Jeong on 2020/10/07.
+//
+
+import SwiftUI
+
+@main
+struct menu_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
